628e0789b9cc6e61341f5e7cdb05574f8cd13e4c
Esben Andreasen <esbenandreasen@gmail.com>
2015-09-09 20:33:16 +0200

    build file for jer.jar

---
+++ scripts/make-log-reader-jar.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+BASE=`pwd`
+SRC="${BASE}/log-readers/java/src"
+DIST="${BASE}/dist"
+BUILD="${BASE}/build"
+
+mkdir -p ${BUILD}
+mkdir -p ${DIST}
+
+cd ${SRC}
+javac -cp ${SRC}/../lib/gson-2.3.1.jar -d ${BUILD} `find ${SRC} -name "*.java"`
+cd ${BUILD}
+jar cf ${DIST}/jer.jar `find . -name "*.class"`
