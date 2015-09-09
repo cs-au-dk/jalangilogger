4fa0e48132c496f174236c563711f1a64e7338dd
Esben Andreasen <esbenandreasen@gmail.com>
2015-09-09 18:04:17 +0200

    scripts

--- scripts/instrumentHTMLFiles.sh
+++ scripts/instrumentHTMLFiles.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-if [ -z ${JALANGI+x} ]; then
-    >&2 echo '$JALANGI is not defined?! Set to e.g. "node_modules/jalangi2"';
-    exit 1;
-fi
 for f in $(find test -name '*.html');
 do
 	echo "$f"
 	saveDir="${f/test/}"    
-    node ${JALANGI}/src/js/commands/instrument.js --inlineIID --inlineSource -i --inlineJalangi --analysis src/ValueLoggerBrowser.js --outputDir instrumentedHtmlFiles/$saveDir "$f"
+    node node_modules/jalangi2/src/js/commands/instrument.js --inlineIID --inlineSource -i --inlineJalangi --analysis src/ValueLoggerBrowser.js --outputDir instrumentedHtmlFiles/$saveDir "$f"
 done
---
+++ scripts/startServer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd nodeJSServer;
+
+node bin/www
