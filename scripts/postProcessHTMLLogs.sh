a41517b08a38ebfb2c296b97cf8c4dccb8948d55
Esben Andreasen <esbenandreasen@gmail.com>
2015-09-09 17:48:01 +0200

    Post-processing of HTML log files with Java

---
+++ scripts/postProcessHTMLLogs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+ROOT="src/java";
+
+LIB="${ROOT}/lib";
+SRC="${ROOT}/src";
+
+javac  -cp ${LIB}/org.json-20120521.jar ${SRC}/dk/au/cs/casa/jer/TransformHtmlLogFiles.java
+java  -cp  ${SRC}:${LIB}/org.json-20120521.jar TransformHtmlLogFiles
