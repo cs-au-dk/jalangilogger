ff7b29e6bf37c68387491de0801f7b449c52c5c7
Esben Andreasen <esbenandreasen@gmail.com>
2015-09-10 09:03:48 +0200

    use environment-aware ValueLogger instead of ValueLoggerBrowser

--- scripts/instrumentHTMLFiles.sh
+++ scripts/instrumentHTMLFiles.sh
@@ -3,5 +3,5 @@ for f in $(find test -name '*.html');
 do
 	echo "$f"
 	saveDir="${f/test/}"    
-    node node_modules/jalangi2/src/js/commands/instrument.js --inlineIID --inlineSource -i --inlineJalangi --analysis src/ValueLoggerBrowser.js --outputDir instrumentedHtmlFiles/$saveDir "$f"
+    node node_modules/jalangi2/src/js/commands/instrument.js --inlineIID --inlineSource -i --inlineJalangi --analysis src/ValueLogger.js --outputDir instrumentedHtmlFiles/$saveDir "$f"
 done
