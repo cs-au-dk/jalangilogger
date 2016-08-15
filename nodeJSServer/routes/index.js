6aeeaa8e70f5e1815c6971159eed32ec93471f80
Esben Andreasen <esbenandreasen@gmail.com>
2016-08-16 00:41:42 +0200

    break existing server (pure Java interface should be used)

--- nodeJSServer/routes/index.js
+++ nodeJSServer/routes/index.js
@@ -12,24 +12,30 @@ router.post('/sendEntries', function(req, res, next) {
 });
 
 router.post('/printToFile', function(req, res, next){
-	res.setHeader('Access-Control-Allow-Origin', 'null');	
+	res.setHeader('Access-Control-Allow-Origin', 'null');
 	res.status(204) //No Content response
 
 	var fs = require('fs');
 	var relativeFilePath = req.body["fileName"];
 	console.log("Printing to file: " + relativeFilePath);
 	var saveDir = "UnchangedLogFiles/" + relativeFilePath.substring(0, relativeFilePath.lastIndexOf("/"));
-	
+
 	var mkdirp = require('mkdirp');
 	mkdirp(saveDir, function (err) {
     	if (err) console.error(err)
     	else {
-    		var file = fs.createWriteStream("UnchangedLogFiles/" + relativeFilePath);
+    	  var file;
+          var USE_OLD_MODE = false;
+          if(USE_OLD_MODE){
+            file = fs.createWriteStream("UnchangedLogFiles/" + relativeFilePath);
+          }else{
+            file = fs.createWriteStream("logfile");
+          }
 			file.on('error', function(err) { console.error(err) });
 			file.on('open', function (fd) {consoleLines.forEach(function(v) { file.write(v + '\n')}); file.end; consoleLines = []});
 		}
 	});
-	
+
 	res.end();
 })
 
