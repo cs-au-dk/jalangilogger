bd2a5ed0869ffdb678c1dc1465dc50073b8f7771
Esben Andreasen <esbenandreasen@gmail.com>
2016-08-28 00:30:52 +0200

    improve CLI

--- logger/src/dk/au/cs/casa/jer/Main.java
+++ logger/src/dk/au/cs/casa/jer/Main.java
@@ -1,37 +1,38 @@
 package dk.au.cs.casa.jer;
 
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
  * Created by torp on 25/08/16.
  */
 public class Main {
 
-    public static void main(String...args) {
-        Path pwd = Paths.get(args[0]);
-        Path testFile = Paths.get(args[1]);
-        Path node = Paths.get("node");
+    public static void main(String... args) throws IOException {
+        Path root = Paths.get(args[0]);
+        Path mainFile = Paths.get(args[1]);
         Logger logger = null;
-        if (args.length > 2) {
+            Path loggerDir;
+        if (args.length > 4) {
             Path testFileDir = Paths.get(args[2]);
-            logger = new Logger(pwd, testFileDir, testFile, node, pwd);
+            Path node = Paths.get(args[3]);
+            loggerDir = Paths.get(args[4]);
+            logger = new Logger(root, testFileDir, mainFile, node, loggerDir);
         } else {
-            logger = new Logger(pwd, testFile, node, pwd);
+            Path node = Paths.get(args[2]);
+            loggerDir = Paths.get(args[3]);
+            logger = new Logger(root, mainFile, node, loggerDir);
         }
-        Path logFile = null;
-        try {
-            logFile = logger.log();
-        } catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
-        }
+        final Path logFile = logger.log();
 
-        Path logFileDestinationFolder = getLogFileDestinationFolder(pwd, testFile);
-        if (!Files.exists(logFileDestinationFolder)) {
-            genDestinationDirIfMission(logFileDestinationFolder);
-        }
-        Path newLogFile = getNewLogFilePath(testFile, logFileDestinationFolder);
+
+        Path logFileDestinationFolder = loggerDir.resolve("JalangiLogFiles").resolve(mainFile).getParent();
+        Path newLogFile = getNewLogFilePath(mainFile, logFileDestinationFolder);
         copyFile(logFile, newLogFile);
+        System.out.println("Log file is located at: " + newLogFile);
     }
 
     private static Path getNewLogFilePath(Path testFile, Path logFileDestinationFolder) {
@@ -39,24 +40,12 @@ public class Main {
         return logFileDestinationFolder.resolve(testFileName.substring(0, testFileName.indexOf('.')) + ".log");
     }
 
-    private static Path getLogFileDestinationFolder(Path pwd, Path testFile) {
-        Path testFileWOTestDir = testFile.subpath(1, testFile.getNameCount());
-        return pwd.resolve("JalangiLogFiles").resolve(testFileWOTestDir.getParent());
-    }
-
     private static void copyFile(Path base, Path destination) {
         try {
+            Files.createDirectories(destination.getParent());
             Files.copy(base, destination, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    private static void genDestinationDirIfMission(Path logFileDestinationFolder) {
-        try {
-            Files.createDirectories(logFileDestinationFolder);
-        } catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
         }
     }
 }
--- scripts/genLogFile.sh
+++ scripts/genLogFile.sh
@@ -3,22 +3,21 @@
 #Stop on error
 set -e
 
-MAIN_FILE=$1
-
+ROOT=$1
+MAIN_FILE=$2
 # The instrumentation will include all files in $dir if specified
-DIR=$2
+DIR=$3
 
-SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT="${SCRIPT_FOLDER}/.."
+PROJECT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 LIB="${PROJECT}/logger/lib"
 DIST="${PROJECT}/logger/dist"
-
+NODE=$(which node)
 JARS=${LIB}/commons-io-2.5.jar:${LIB}/gson-2.3.1.jar:${LIB}/org.json-20120521.jar:${DIST}/jer.jar
 
 MAIN_CLASS=dk.au.cs.casa.jer.Main
 
 if [[ -z $DIR ]]; then
-    java -cp $JARS $MAIN_CLASS $PROJECT $MAIN_FILE 
+    java -cp $JARS $MAIN_CLASS $ROOT $MAIN_FILE $NODE $PROJECT
 else
-    java -cp $JARS $MAIN_CLASS $PROJECT $MAIN_FILE $DIR
+    java -cp $JARS $MAIN_CLASS $ROOT $MAIN_FILE $DIR $NODE $PROJECT 
 fi
