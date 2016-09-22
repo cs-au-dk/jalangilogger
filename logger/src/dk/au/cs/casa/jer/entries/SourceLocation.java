9dca3c81813be3a893fbc23a38afae550628ed2c
Esben Andreasen <esbenandreasen@gmail.com>
2016-09-23 00:30:49 +0200

    add equals/hashcode to SourceLocation

--- logger/src/dk/au/cs/casa/jer/entries/SourceLocation.java
+++ logger/src/dk/au/cs/casa/jer/entries/SourceLocation.java
@@ -8,39 +8,60 @@ public class SourceLocation {
 
     private String fileName;
 
-    @Override
-    public String toString() {
-        return String.format("%s:%d:%d", fileName, lineNumber, columnNumber);
-    }
-
-    public SourceLocation (int lineNumber, int columnNumber, String fileName) {
-
+    public SourceLocation(int lineNumber, int columnNumber, String fileName) {
         this.lineNumber = lineNumber;
         this.columnNumber = columnNumber;
         this.fileName = fileName;
     }
 
-    public int getColumnNumber () {
+    @Override
+    public String toString() {
+        return String.format("%s:%d:%d", fileName, lineNumber, columnNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final SourceLocation that = (SourceLocation) o;
+
+        if (lineNumber != that.lineNumber) return false;
+        if (columnNumber != that.columnNumber) return false;
+        if (fileName != null ? !fileName.equals(that.fileName) : that.fileName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = lineNumber;
+        result = 31 * result + columnNumber;
+        result = 31 * result + (fileName != null ? fileName.hashCode() : 0);
+        return result;
+    }
+
+    public int getColumnNumber() {
         return columnNumber;
     }
 
-    public void setColumnNumber (int columnNumber) {
+    public void setColumnNumber(int columnNumber) {
         this.columnNumber = columnNumber;
     }
 
-    public String getFileName () {
+    public String getFileName() {
         return fileName;
     }
 
-    public void setFileName (String fileName) {
+    public void setFileName(String fileName) {
         this.fileName = fileName;
     }
 
-    public int getLineNumber () {
+    public int getLineNumber() {
         return lineNumber;
     }
 
-    public void setLineNumber (int lineNumber) {
+    public void setLineNumber(int lineNumber) {
         this.lineNumber = lineNumber;
     }
 }
