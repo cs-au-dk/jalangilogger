28f03033ba45da2a06dec249f0265d5f126ea63e
Esben Andreasen <esbenandreasen@gmail.com>
2016-09-28 20:52:08 +0200

    fixup: type errors

--- logger/src/dk/au/cs/casa/jer/Main.java
+++ logger/src/dk/au/cs/casa/jer/Main.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by torp on 25/08/16.
@@ -15,17 +17,18 @@ public class Main {
         Logger logger;
         List<Path> preambles = new ArrayList<>();
         int defaultTimeLimit = 60;
+        Set<Path> onlyInclude = new HashSet<>();
         if (args.length > 4) {
             Path mainFile = Paths.get(args[0]);
             Path node = Paths.get(args[1]);
             Path loggerDir = Paths.get(args[2]);
-            logger = Logger.makeLoggerForIndependentMainFile(mainFile, preambles, defaultTimeLimit, getEnvironment(mainFile), node, loggerDir, null);
+            logger = Logger.makeLoggerForIndependentMainFile(mainFile, preambles, onlyInclude, defaultTimeLimit, getEnvironment(mainFile), node, loggerDir, null);
         } else {
             Path root = Paths.get(args[0]);
             Path mainFile = Paths.get(args[1]);
             Path node = Paths.get(args[2]);
             Path loggerDir = Paths.get(args[3]);
-            logger = Logger.makeLoggerForDirectoryWithMainFile(root, mainFile, preambles, defaultTimeLimit, getEnvironment(mainFile), node, loggerDir, null);
+            logger = Logger.makeLoggerForDirectoryWithMainFile(root, mainFile, preambles, onlyInclude, defaultTimeLimit, getEnvironment(mainFile), node, loggerDir, null);
         }
         final Path logFile = logger.log();
         System.out.println("Log file is located at: " + logFile);
